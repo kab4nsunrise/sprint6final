6550c2cb7154a9f8fe4e8d318858e418dc98833f
Артем <kotbegemot001@yandex.ru>
2026-02-24 14:58:57 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server" 
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 	logger := log.New(os.Stdout, "MORSE-CONVERTER: ", log.LstdFlags|log.Lshortfile)
 
 	
-	srv := server.NewServer(logger)
+	srv := server.NewServer(logger) 
 
 	
 	if err := srv.Run(); err != nil {
