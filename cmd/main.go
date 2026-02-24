d0d7d544395d7db9a1bde0bde2717fe65033cb34
Артем <kotbegemot001@yandex.ru>
2026-02-24 14:48:15 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,17 @@ import (
 	"log"
 	"os"
 
-	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server" 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
 
 func main() {
-	// Создаем логгер
+	
 	logger := log.New(os.Stdout, "MORSE-CONVERTER: ", log.LstdFlags|log.Lshortfile)
 
-	// Создаем сервер
+	
 	srv := server.NewServer(logger)
 
-	// Запускаем сервер
+	
 	if err := srv.Run(); err != nil {
 		logger.Fatal("Ошибка при запуске сервера:", err)
 	}
